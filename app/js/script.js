092e06393bf780b986385831cead75b41dde38a6
Tuhin Roy <troy0870@gmail.com>
2021-04-04 09:06:03 +0530

    add edges for screen resolutions

--- app/js/script.js
+++ app/js/script.js
@@ -3,17 +3,13 @@ const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade')
+const mobileAbout = document.querySelector('#about');
+
+
 
 btnHamburger.addEventListener('click', function () {
     if (header.classList.contains('open')) {
-        body.classList.remove('noscroll');
-        header.classList.remove('open');
-        fadeElems.forEach(function (element) {
-            element.classList.remove('fade-in')
-            element.classList.add('fade-out')
-        })
-        overlay.classList.remove('fade-in')
-        overlay.classList.add('fade-out')
+        removeMobileNavStyles();
     } else {
         body.classList.add('noscroll');
         header.classList.add('open');
@@ -23,3 +19,28 @@ btnHamburger.addEventListener('click', function () {
         })
     }
 })
+
+mobileAbout.addEventListener('click', function () {
+    if (header.classList.contains('open')) {
+        removeMobileNavStyles();
+        document.querySelector('#feature').scrollIntoView();
+    }
+})
+
+window.addEventListener('resize', function (event) {
+    if (window.innerWidth >= 1024 && header.classList.contains('open')) {
+        removeMobileNavStyles();
+    }
+})
+
+const removeMobileNavStyles = () => {
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
+        element.classList.remove('fade-in')
+        element.classList.add('fade-out')
+    })
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+}
+
